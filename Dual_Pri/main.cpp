9650d1ec7556044eb12b8e5b427aee8998234e68
GuXiaozhe <sunshine8641@guxiaozhes-macbook.local>
2016-03-09 14:00:38 +0800

    Initial Commit

---
+++ Dual_Pri/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Dual_Pri
+//
+//  Created by GuXiaozhe on 9/3/16.
+//  Copyright © 2016 GuXiaozhe. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
